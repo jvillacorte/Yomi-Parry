280aae8ef45bc0ef90e1af1d4bfb04ac388b3d3b
jvillacorte <joseph.villacorte@gmail.com>
2025-09-23 07:35:40 -0700

    Player Addition

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+
+    float horizontalMovement;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+}
